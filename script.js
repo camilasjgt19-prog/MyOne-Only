3477b5de99ceffa117f1bc61f59fc2bbba9da335
camilasjgt19-prog <camilasjgt19@gmail.com>
2026-02-09 19:06:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const frases = [
     "Para mi niña bonita, con mucho cariño",
-    "El primer día de cita en el parque, el día de nuestro primer beso. Desde ese día amo acostarme debajo de los árboles a sentir la frescura de ellos",
-    "Los parques representan momentos especiales, como el día en el que nos hicieron pololindas",
-    "Los días en casa se han hecho mis favoritos para estar juntas",
+    "El primer día de cita en el parque, el día de nuestro primer beso. Desde ese día amo ir contigo a esos lugares",
+    "Los parques han representado momentos especiales, como el día en el que nos hicimos pololindas",
+    "Los días en casa se han hecho mis favoritos para estar contigo",
     "Cada salida contigo es formar un nuevo recuerdo especial",
     "Cada vez que me miras fijamente y me das un besito, siento que el mundo se detiene y solo existimos tú y yo",
-    "Los momentos de familia tradicional me hacen sentir que tu eres mi hogar",
+    "Los momentos en familia tradicional me hacen sentir que tú eres mi hogar",
     "La cantidad de cariño y amor que emanas me hace sentir la persona más especial del mundo",
-    "Tu sonrisa y tus vibes tan lindas me alegran mi vida",
-    "Cuando vuelvo a mirar todas las fotos que he tomado de nosotras no puedo evitar sentirme tan feliz por tener esos recuerdos contigo",
-    "Irnos de viaje en nuestros aniversarios son los momentos que mas espero y la tradición que mas amo de nosotras",
-    "Cada beso que me das, simple, suave, apasionado o profundo me hace sentir la persona mas amada y enamorada",
+    "Tu sonrisa y tus vibes tan lindas me alegran la vida",
+    "Cuando vuelvo a mirar todas las fotos que he tomado de nosotras, no puedo evitar sentirme tan feliz por tener esos recuerdos contigo",
+    "Irnos de viaje de aniversario es el momento que más espero y la tradición que más me gusta de nosotras",
+    "Cada beso que me das, simple, suave, apasionado o profundo, me hace sentir la persona más enamorada",
     "Te quiero dar millones de besos y abrazos por el resto de mi vida",
-    "El amor que sienti por ti se refleja en mi día a día y en lo mucho que cquiero cuidar de tu corazón",
+    "El amor que siento por ti se refleja en mi día a día, y en lo mucho que deseo cuidar de tu corazón",
     "Te veo y solo puedo pensar en que eres la persona más maravillosa y hermosa del mundo ",
-    "Quiero llevarte conmigo a donde vaya porque eres mi lugar favorito",
-    "Para la persona más linda y preciosa y bonita y especial e increible del mundo, te amo mi ani",
+    "Quiero llevarte conmigo a donde vaya, porque eres mi lugar favorito",
+    "Para la persona más linda, preciosa, bonita, especial e increible del mundo, te amo mí ani",
     "Eres el amor de mi vida, la mujer de mis sueños, la persona de mis anhelos, simplemente, eres mi todo"
 ];
 
@@ -65,6 +65,7 @@ function lanzarDecoracion() {
         setTimeout(() => { el.remove(); }, 3000);
     }
 }
+
 
 
 
